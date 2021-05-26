15d7cddc26334e4e8e4c646e8159cb5dfab8fb05
David <davidhand1988@gmail.com>
2021-05-26 01:19:27 -0400

    [WIP] Add structs for createMetaOrder endpoint.

--- cmd/kraken-meta-order/main.go
+++ cmd/kraken-meta-order/main.go
@@ -3,12 +3,51 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
+
+	"encoding/json"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+type MetaOrder struct {
+	MetaOrderId        int                `json:"metaOrderId"`
+	MetaOrderType      string             `json:"metaOrderType"`
+	Status             string             `json:"status"`
+	StopLossTakeProfit StopLossTakeProfit `json:"stopLossTakeProfit"`
+	CreateDateTime     time.Time          `json:"createDateTime"`
+	CreateUserName     string             `json:"createUserName"`
+	LastUpdateDateTime time.Time          `json:"lastUpdateDateTime"`
+	LastUpdateUserName string             `json:"lastUpdateUserName"`
+}
+
+type StopLossTakeProfit struct {
+	StopLossPrice   float32 `json:"stopLossPrice"`
+	TakeProfitPrice float32 `json:"takeProfitPrice"`
+}
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	respondWithJSON(w, code, map[string]string{"error": message})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
 func createMetaOrder(w http.ResponseWriter, r *http.Request) {
+	var metaOrder MetaOrder
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&metaOrder); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	defer r.Body.Close()
 	fmt.Fprint(w, "Create Order")
 }
 
